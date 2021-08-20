c40523ffa498868557946940ff60bb915aace210
Emilio P <ejpascualj@gmail.com>
2021-08-19 19:06:04 -0500

    modified models

--- models/workout.js
+++ models/workout.js
@@ -1,39 +1,10 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: () => new Date()
-    },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter a type"
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter a name"
-        },
-        duration: {
-            type: Number,
-            required: "Enter a duration"
-        },
-        weight: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        }
-    }],
+    day: Date,
+    exercises: []
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
 module.exports = Workout;
