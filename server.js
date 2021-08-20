983bb4de0334d1565f7a21eb6ec7aa6c802a5ed0
Emilio P <ejpascualj@gmail.com>
2021-08-19 19:05:40 -0500

    modified server.js

--- server.js
+++ server.js
@@ -1,29 +1,92 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path");
+
 
 const PORT = process.env.PORT || 3000;
-
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// Mongoose Database Connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.get("/api/workouts", (req, res) => {
+
+  db.Workout.aggregate( [
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" }
+      }
+    }
+ ] ).then(db.Workout.find({}))
+ .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  
+    db.Workout.aggregate( [
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" }
+      }
+    }
+ ] ).then(db.Workout.find({}))
+ .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate({ _id: req.params.id }, {
+    $push: { exercises: req.body }}, { new: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+app.post("/api/workouts/", (req, res) => {
+  db.Workout.create({day: new Date(new Date().setDate(new Date().getDate()))})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
